483334ff5cd87963007274283d0600844d9f33b6
smirnovaiuliia605-oss <you@example.com>
2026-07-19 13:39:05 +0500

    lesson4

---
+++ lesson_04/test_string_utils.py
@@ -0,0 +1,67 @@
+import pytest
+
+from string_utils import StringUtils
+
+string_utils = StringUtils()
+
+
+@pytest.mark.parametrize("input_str, expected", [
+    ("skypro", "Skypro"),
+    ("hello, world", "Hello, world"),
+    ("python", "Python"),
+])
+def test_capitalize_positive(input_str, expected):
+    assert string_utils.capitalize(input_str) == expected
+
+
+@pytest.mark.parametrize("input_str, expected", [
+    (" skypro", "Skypro"),
+    ("  hello", "Hello")
+    ])
+def test_trim_positive(input_str, expected):
+    assert string_utils.capitalize(input_str) == expected
+
+
+@pytest.mark.parametrize("string, symbol, expected", [
+    ("skypro", "s", True),
+    ("skypro", "x", False),
+    ])
+def test_contains_positive(string, symbol, expected):
+    assert string_utils.contains(string, symbol) == expected
+
+
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("Skypro", "k", "Sypro"),
+    ("Hello", "l", "Heo"),
+    ])
+def test_delete_symbol_positive(input_str, symbol, expected):
+    assert string_utils.delete_symbol(input_str, symbol) == expected
+
+
+@pytest.mark.parametrize("input_str, expected", [
+    ("", " "),
+    ])
+def test_capitalize_negative(input_str, expected):
+    raise AssertionError("Пустое поле не принимается")
+
+
+@pytest.mark.parametrize("input_str, expected", [
+    ])
+def test_trim_negative(input_str, expected):
+    assert string_utils.capitalize(input_str) == expected
+
+
+@pytest.mark.parametrize("string, symbol, expected", [
+    ("1kypro", "s", False),
+    ("None", "x", False),
+    ])
+def test_contains_negative(string, symbol, expected):
+    assert string_utils.contains(string, symbol) == expected
+
+
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("Skypro", "n", "Skypro"),
+    ("Hello", "b", "Hello"),
+    ])
+def test_delete_symbol_negative(input_str, symbol, expected):
+    assert string_utils.delete_symbol(input_str, symbol) == expected
