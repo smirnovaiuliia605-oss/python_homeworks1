7a397ee0107176420bf2ddb2df4fc7519ba61152
smirnovaiuliia605-oss <you@example.com>
2026-07-20 17:45:29 +0500

    04_lesson

--- 04_lesson/test_string_utils.py
+++ 04_lesson/test_string_utils.py
@@ -15,11 +15,11 @@ def test_capitalize_positive(input_str, expected):
 
 
 @pytest.mark.parametrize("input_str, expected", [
-    (" skypro", "Skypro"),
-    ("  hello", "Hello")
+    (" skypro", "skypro"),
+    ("  hello", "hello")
     ])
 def test_trim_positive(input_str, expected):
-    assert string_utils.capitalize(input_str) == expected
+    assert string_utils.trim(input_str) == expected
 
 
 @pytest.mark.parametrize("string, symbol, expected", [
@@ -40,15 +40,19 @@ def test_delete_symbol_positive(input_str, symbol, expected):
 
 @pytest.mark.parametrize("input_str, expected", [
     ("", " "),
+    (None, " ")
     ])
 def test_capitalize_negative(input_str, expected):
-    raise AssertionError("Пустое поле не принимается")
+    assert string_utils.capitalize("") == ""
 
 
-@pytest.mark.parametrize("input_str, expected", [
+@pytest.mark.parametrize("invalid_input", [
+    None,
+    123,
     ])
-def test_trim_negative(input_str, expected):
-    assert string_utils.capitalize(input_str) == expected
+def test_trim_negative_invalid_types(invalid_input):
+    with pytest.raises(AttributeError):
+        string_utils.trim(invalid_input)
 
 
 @pytest.mark.parametrize("string, symbol, expected", [
@@ -59,9 +63,25 @@ def test_contains_negative(string, symbol, expected):
     assert string_utils.contains(string, symbol) == expected
 
 
+@pytest.mark.parametrize("invalid_input", [
+    (None, "y", False),
+    ])
+def test_contains_negative_invalid_types(invalid_input):
+    with pytest.raises(AttributeError):
+        string_utils.trim(invalid_input)
+
+
 @pytest.mark.parametrize("input_str, symbol, expected", [
     ("Skypro", "n", "Skypro"),
     ("Hello", "b", "Hello"),
     ])
 def test_delete_symbol_negative(input_str, symbol, expected):
     assert string_utils.delete_symbol(input_str, symbol) == expected
+
+
+@pytest.mark.parametrize("invalid_input", [
+    None,
+    ])
+def test_delete_simbol_negative_invalid_types(invalid_input):
+    with pytest.raises(AttributeError):
+        string_utils.trim(invalid_input)
