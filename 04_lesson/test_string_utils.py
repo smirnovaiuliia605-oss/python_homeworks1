3f272a9cbdbac2b4cba5ab1f7b62f32e1fcfab19
smirnovaiuliia605-oss <you@example.com>
2026-07-20 17:59:28 +0500

    lesson4

--- 04_lesson/test_string_utils.py
+++ 04_lesson/test_string_utils.py
@@ -5,6 +5,7 @@ from string_utils import StringUtils
 string_utils = StringUtils()
 
 
+@pytest.mark.positive
 @pytest.mark.parametrize("input_str, expected", [
     ("skypro", "Skypro"),
     ("hello, world", "Hello, world"),
@@ -14,6 +15,7 @@ def test_capitalize_positive(input_str, expected):
     assert string_utils.capitalize(input_str) == expected
 
 
+@pytest.mark.positive
 @pytest.mark.parametrize("input_str, expected", [
     (" skypro", "skypro"),
     ("  hello", "hello")
@@ -22,6 +24,7 @@ def test_trim_positive(input_str, expected):
     assert string_utils.trim(input_str) == expected
 
 
+@pytest.mark.positive
 @pytest.mark.parametrize("string, symbol, expected", [
     ("skypro", "s", True),
     ("skypro", "x", False),
@@ -30,6 +33,7 @@ def test_contains_positive(string, symbol, expected):
     assert string_utils.contains(string, symbol) == expected
 
 
+@pytest.mark.positive
 @pytest.mark.parametrize("input_str, symbol, expected", [
     ("Skypro", "k", "Sypro"),
     ("Hello", "l", "Heo"),
@@ -38,6 +42,7 @@ def test_delete_symbol_positive(input_str, symbol, expected):
     assert string_utils.delete_symbol(input_str, symbol) == expected
 
 
+@pytest.mark.negative
 @pytest.mark.parametrize("input_str, expected", [
     ("", " "),
     (None, " ")
@@ -46,6 +51,7 @@ def test_capitalize_negative(input_str, expected):
     assert string_utils.capitalize("") == ""
 
 
+@pytest.mark.negative
 @pytest.mark.parametrize("invalid_input", [
     None,
     123,
@@ -55,6 +61,7 @@ def test_trim_negative_invalid_types(invalid_input):
         string_utils.trim(invalid_input)
 
 
+@pytest.mark.negative
 @pytest.mark.parametrize("string, symbol, expected", [
     ("1kypro", "s", False),
     ("None", "x", False),
@@ -63,6 +70,7 @@ def test_contains_negative(string, symbol, expected):
     assert string_utils.contains(string, symbol) == expected
 
 
+@pytest.mark.negative
 @pytest.mark.parametrize("invalid_input", [
     (None, "y", False),
     ])
@@ -71,6 +79,7 @@ def test_contains_negative_invalid_types(invalid_input):
         string_utils.trim(invalid_input)
 
 
+@pytest.mark.negative
 @pytest.mark.parametrize("input_str, symbol, expected", [
     ("Skypro", "n", "Skypro"),
     ("Hello", "b", "Hello"),
@@ -79,6 +88,7 @@ def test_delete_symbol_negative(input_str, symbol, expected):
     assert string_utils.delete_symbol(input_str, symbol) == expected
 
 
+@pytest.mark.negative
 @pytest.mark.parametrize("invalid_input", [
     None,
     ])
